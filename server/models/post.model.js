56067628faf9989c060e9f22ac15ab8a5307eceb
raj128 <rajgautam043@gmail.com>
2023-10-18 01:19:54 +0530

    post model update
    donee added

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -3,48 +3,54 @@ import mongoose from 'mongoose';
 const { ObjectId } = mongoose.Schema.Types;
 
 const postSchema = new mongoose.Schema(
-	{
-		title: {
-			type: String,
-			required: true,
-		},
-		venue: {
-			type: String,
-			required: true,
-		},
-		city: {
-			type: String,
-			required: true,
-		},
-		img_url: {
-			type: String,
-			default:"#"
-		},
-		postedBy:{
-			type:ObjectId,
-			ref:"user"
-		},
-		food_tags:{type:String},
-        meal_size:{
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        venue: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        img_url: {
+            type: String,
+            default: "#"
+        },
+        postedBy: {
+            type: ObjectId,
+            ref: "user"
+        },
+        food_tags: { type: String },
+        meal_size: {
             type: Number,
         },
-		likes: [{ type: ObjectId, ref: "user" }],
-		booker: [{ type: ObjectId, ref: "user" }],
-		comments: [
-			{
-				Text: String,
-				postedBy: {
-					type: ObjectId,
-					ref: "user",
-				},
-			},
-		],
-		expirey_date:{
-			type:Date,
-		}
-	},
-	{ timestamps: true }
+        likes: [{ type: ObjectId, ref: "user" }],
+        donees: [
+            {
+                donee: { type: ObjectId, ref: "user" },
+                bookingTime: { type: Date, default: Date.now }
+            }
+        ],
+        comments: [
+            {
+                Text: String,
+                postedBy: {
+                    type: ObjectId,
+                    ref: "user",
+                },
+            },
+        ],
+        expirey_date: {
+            type: Date,
+        }
+    },
+    { timestamps: true }
 );
-var PostModel = mongoose.model('posts', postSchema);
 
-export default PostModel;
+const PostModel = mongoose.model('posts', postSchema);
+
+export default PostModel;
